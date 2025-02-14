c4a2bbec84b4f8fd841dc966fd854d0342f1d4fa
Revitte <51162772+Revitte@users.noreply.github.com>
2025-02-14 10:40:47 -0300

    Estrutura Spring Sendo implementada
    Dependências:
    
    ✅ Spring Web
    ✅ Spring Data JPA
    ✅ H2 Database
    ✅ Lombok
    
    Adicionadas ao projeto.

---
+++ agendamento-transferencias/src/test/java/com/jazz/agendamento/AgendamentoTransferenciasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jazz.agendamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendamentoTransferenciasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
